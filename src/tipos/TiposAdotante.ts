4ec7f32204ab37e9f47a1bb644cb2a0452f00817
EmersonLaranja <emersonlaranja@gmail.com>
2023-10-23 17:10:42 -0300

    1.1

--- src/tipos/TiposAdotante.ts
+++ src/tipos/TiposAdotante.ts
@@ -3,17 +3,11 @@ import AdotanteEntity from "../entities/AdotanteEntity";
 type TipoRequestBodyAdotante = Omit<AdotanteEntity, "id">;
 
 type TipoResponseBodyAdotante = {
-  data?: Pick<AdotanteEntity, "nome" | "celular">;
+  data?:
+    | Pick<AdotanteEntity, "id" | "nome" | "celular">
+    | Pick<AdotanteEntity, "id" | "nome" | "celular">[];
 
   error?: unknown;
 };
 
-type TipoRequestParamsAdotante = {
-  id: string;
-};
-
-export {
-  TipoRequestBodyAdotante,
-  TipoRequestParamsAdotante,
-  TipoResponseBodyAdotante,
-};
+export { TipoRequestBodyAdotante, TipoResponseBodyAdotante };
