172c6f6cc0d2d6214666b23c7effcc34b9db070c
EmersonLaranja <emersonlaranja@gmail.com>
2023-10-23 16:35:17 -0300

    1.1

--- src/tipos/TiposAdotante.ts
+++ src/tipos/TiposAdotante.ts
@@ -3,7 +3,7 @@ import AdotanteEntity from "../entities/AdotanteEntity";
 type TipoRequestAdotante = Omit<AdotanteEntity, "id">;
 
 type TipoResponseAdotante = {
-  data: Pick<AdotanteEntity, "nome" | "celular">;
+  data?: Pick<AdotanteEntity, "nome" | "celular">;
 
   error?: {
     mensagem: string;
