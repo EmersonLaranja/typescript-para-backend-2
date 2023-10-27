f490b9980597c4661e6e365cafdc19615fdef717
EmersonLaranja <emersonlaranja@gmail.com>
2023-10-27 07:32:33 -0300

    2.2

--- src/controller/AdotanteController.ts
+++ src/controller/AdotanteController.ts
@@ -29,7 +29,13 @@ export default class AdotanteController {
       bodyValidated = await adotanteBodyValidator.validate(req.body,{abortEarly:false});
     } catch (error) {
       const yupErrors = error as yup.ValidationError;
-      return res.status(400).json({ error: yupErrors.message });
+      
+      const validationErrors:Record<string,string>={}
+      yupErrors.inner.forEach(error=>{
+        if(!error.path) return;
+        validationErrors[error.path]=error.message
+      })
+      return res.status(400).json({ error: validationErrors });
     }
 
     const { nome, celular, endereco, foto, senha } = bodyValidated;
